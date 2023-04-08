abea36a6e900c919a1ccc14dc7e54cae99e3f8ad
Bruno Cavalcante <126831446+Brunoensaboa@users.noreply.github.com>
2023-04-08 19:48:23 -0300

    Update App.java

--- lista01_java_exerc_13/src/App.java
+++ lista01_java_exerc_13/src/App.java
@@ -1,6 +1,7 @@
 package lista01_java_exerc_13.src;
 //Tendo como dados de entrada a altura de uma pessoa, construa um
-// algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+// algoritmo que calcule seu peso ideal, usando a seguinte fórmula:Para homens: (72.7*h) - 58
+//Para mulheres: (62.1*h) - 44.7
 
 
 
